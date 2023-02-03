cdabf26f8d50d39621ed6767aa9566f3c1f3cc4a
Ivan Saavedra <ivan.saavedra.iezs@gmail.com>
2023-02-03 00:55:43 -0600

    se le agrego interaccion a la imagen para mostrar los detalles del producto y que se cerrara al presionar un boton. Se arreglo el problema de superposision con los menus (mobil y desktop)

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 function toggleDesktopMenu() {
     const menuMyOrderClosed = menuMyOrder.classList.contains('inactive');
     const menuMobileClosed = mobileMenu.classList.contains('inactive');
+    const productDtlContClosed = productDtlContainer.classList.contains('inactive');
 
     if (!menuMyOrderClosed) {
         menuMyOrder.classList.add('inactive');
@@ -14,6 +15,10 @@ function toggleDesktopMenu() {
 
     if (!menuMobileClosed) {
         mobileMenu.classList.add('inactive');
+    }
+
+    if(!productDtlContClosed){
+        productDtlContainer.classList.add('inactive');
     }
 
     desktopMenu.classList.toggle('inactive');
@@ -28,6 +33,7 @@ menuIcon.addEventListener('click', toggleMobileMenu);
 function toggleMobileMenu() {
     const menuMyOrderClosed = menuMyOrder.classList.contains('inactive');
     const menuDesktopClosed = desktopMenu.classList.contains('inactive');
+    const productDtlContClosed = productDtlContainer.classList.contains('inactive');
 
     if (!menuDesktopClosed) {
         desktopMenu.classList.add('inactive');
@@ -37,10 +43,14 @@ function toggleMobileMenu() {
         menuMyOrder.classList.add('inactive');
     }
 
+    if(!productDtlContClosed){
+        productDtlContainer.classList.add('inactive');
+    }
+
     mobileMenu.classList.toggle('inactive');
 }
 
-/*~~~~~~~~~~~~~~~~  MOBILE MENU - NAVBAR  ~~~~~~~~~~~~~~~~~~ */
+/*~~~~~~~~~~~~~~~~  MY ORDER, SHOPPING CART   ~~~~~~~~~~~~~~~~~~ */
 const menuMyOrder = document.querySelector('.product-detail');
 const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
 
@@ -49,6 +59,7 @@ shoppingCartIcon.addEventListener('click', toggleShoppingCart);
 function toggleShoppingCart() {
     const menuMobileClosed = mobileMenu.classList.contains('inactive');
     const menuDesktopClosed = desktopMenu.classList.contains('inactive');
+    const productDtlContClosed = productDtlContainer.classList.contains('inactive');
 
     if (!menuDesktopClosed) {
         desktopMenu.classList.add('inactive');
@@ -56,71 +67,101 @@ function toggleShoppingCart() {
     if (!menuMobileClosed) {
         mobileMenu.classList.add('inactive');
     }
+    if(!productDtlContClosed){
+        productDtlContainer.classList.add('inactive');
+    }
 
     menuMyOrder.classList.toggle('inactive');
 }
 
 /*~~~~~~~~~~~~~~~~  PRODUCT LIST  ~~~~~~~~~~~~~~~~~~ */
+const productDtlContainer = document.querySelector('.product-dtl2');
+const productDtlCloseBtn = document.querySelector('.product-dtl2-close');
+
+productDtlCloseBtn.addEventListener('click',closeProductDtl2);
+
+function openProductDtl2()  {
+    const menuDesktopClosed = desktopMenu.classList.contains('inactive');
+    const menuMobileClosed = mobileMenu.classList.contains('inactive');
+    const menuMyOrderClosed = menuMyOrder.classList.contains('inactive');
+
+    if (!menuDesktopClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+    if (!menuMobileClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!menuMyOrderClosed) {
+        menuMyOrder.classList.add('inactive');
+    }
+
+    productDtlContainer.classList.remove('inactive');
+}
+
+function closeProductDtl2() {
+    productDtlContainer.classList.add('inactive');
+}
+
 const productList = [];
 
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 01',
+    price: 199,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 02',
+    price: 760,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 03',
+    price: 99,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 04',
+    price: 780,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 05',
+    price: 303,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 06',
+    price: 210,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 07',
+    price: 159,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 08',
+    price: 200,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 09',
+    price: 1900,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 10',
+    price: 1201,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 11',
+    price: 420,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 productList.push({
-    name: 'Bike',
-    price: 120,
+    name: 'Bike 12',
+    price: 590,
     image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
 });
 
@@ -135,6 +176,7 @@ for(product in productList){
 
     const productImg = document.createElement('img');
     productImg.setAttribute('src', productList[product].image);
+    productImg.addEventListener('click',openProductDtl2);
 
     const productInfo = document.createElement('div');
     productInfo.classList.add('product-info');
